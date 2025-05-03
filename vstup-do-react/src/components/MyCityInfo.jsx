c53e60b29daf79fa942ddbfa9173d48141d1be2a
Bohdan <kbodia80@gmail.com>
2025-05-03 11:49:54 +0200

    Домашнє завдання 02.05.2025 — React :: init (Завдання 1–4)

---
+++ vstup-do-react/src/components/MyCityInfo.jsx
@@ -0,0 +1,11 @@
+function MyCityInfo() {
+    return(
+        <div>
+            <p>Країна: Україна</p>
+            <p>Область: Херсонська область</p>
+            <p>Рік заснування: 1709</p>
+        </div>
+    )
+}
+
+export default MyCityInfo;
---
+++ vstup-do-react/src/components/MyLandMarks.jsx
@@ -0,0 +1,21 @@
+function MyLandMarks(){
+    return(
+        <div>
+            <h2>Пам'ятки:</h2>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Hola_Prystan_Monument_of_T.Shevchenko_02_%28YDS_0723%29.jpg/250px-Hola_Prystan_Monument_of_T.Shevchenko_02_%28YDS_0723%29.jpg"
+                alt="Пам’ятник Тарасові Шевченку в Голій Пристані"
+                width="400"
+                style={{ margin: '20px' }}
+            />
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Hola_Prystan%2C_a_central_seafront_-_panoramio.jpg/500px-Hola_Prystan%2C_a_central_seafront_-_panoramio.jpg"
+                alt="Пірс і пам’ятник кораблю в Голій Пристані"
+                width="400"
+                style={{ margin: '20px' }}
+            />
+        </div>
+    )
+}
+
+export default MyLandMarks;
